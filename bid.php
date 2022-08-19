427599400e9d218ffa87dfcdad83d48940b806e7
Md. Labib Hasan <59532072+Labib25@users.noreply.github.com>
2022-08-19 11:33:42 +0600

    bid added

---
+++ bid.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Bidding Page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            font-family: sans-serif;
+        }
+
+        header {
+            width: 100%;
+        }
+
+        nav {
+            width: 100%;
+            height: 60px;
+            background: #4bcffa;
+        }
+
+        .logo {
+            font-size: 32px;
+            line-height: 60px;
+            margin-left: 20px;
+            color: #fff;
+            text-align: center;
+        }
+
+        header img {
+            width: 100%;
+            height: calc(100vh - 60px);
+            background-size: cover;
+            background-repeat: no-repeat;
+            object-fit: cover;
+        }
+
+        .section h1 {
+            margin: 40px 0px;
+            text-align: center;
+            font-weight: 600;
+        }
+
+        table {
+            width: 480px;
+            font-size: 28px;
+            margin: 20px auto;
+            background-color: #e6f2ff;
+            border-radius: 10px;
+            padding: 10px;
+        }
+
+        table tr td button {
+            padding: 5px 20px;
+            font-size: 18px;
+            background: #004d00;
+            color: #fff;
+            border: none;
+            border-radius: 5px;
+            margin: 5px;
+        }
+
+        @media screen and (max-width: 768px) {
+            table {
+                width: 320px;
+                font-size: 24px;
+                margin: 15px auto;
+                border-radius: 10px;
+                padding: 5px;
+            }
+        }
+
+        @media screen and (max-width: 480px) {
+            table {
+                width: 250px;
+                font-size: 18px;
+                margin: 10px auto;
+                border-radius: 10px;
+                padding: 5px;
+            }
+        }
+
+        * {
+            box-sizing: border-box
+        }
+
+        body {
+            font-family: Verdana, sans-serif;
+            margin: 0
+        }
+
+        .mySlides {
+            display: none
+        }
+
+        img {
+            vertical-align: middle;
+        }
+
+        .slideshow-container {
+            max-width: 1000px;
+            position: relative;
+            margin: auto;
+        }
+
+        .prev,
+        .next {
+            cursor: pointer;
+            position: absolute;
+            top: 50%;
+            width: auto;
+            padding: 16px;
+            margin-top: -22px;
+            color: white;
+            font-weight: bold;
+            font-size: 18px;
+            transition: 0.6s ease;
+            border-radius: 0 3px 3px 0;
+            user-select: none;
+        }
+
+        .next {
+            right: 0;
+            border-radius: 3px 0 0 3px;
+        }
+
+        .prev:hover,
+        .next:hover {
+            background-color: rgba(0, 0, 0, 0.8);
+        }
+
+        .text {
+            color: #f2f2f2;
+            font-size: 15px;
+            padding: 8px 12px;
+            position: absolute;
+            bottom: 8px;
+            width: 100%;
+            text-align: center;
+        }
+
+        .numbertext {
+            color: #f2f2f2;
+            font-size: 12px;
+            padding: 8px 12px;
+            position: absolute;
+            top: 0;
+        }
+
+        .dot {
+            cursor: pointer;
+            height: 15px;
+            width: 15px;
+            margin: 0 2px;
+            background-color: #bbb;
+            border-radius: 50%;
+            display: inline-block;
+            transition: background-color 0.6s ease;
+        }
+
+        .active,
+        .dot:hover {
+            background-color: #717171;
+        }
+
+        .fade {
+            animation-name: fade;
+            animation-duration: 1.5s;
+        }
+
+        @keyframes fade {
+            from {
+                opacity: .4
+            }
+
+            to {
+                opacity: 1
+            }
+        }
+
+        @media only screen and (max-width: 300px) {
+
+            .prev,
+            .next,
+            .text {
+                font-size: 11px
+            }
+        }
+    </style>
+</head>
+
+<body>
+
+    <div class="slideshow-container">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="https://onde-website-bucket.s3.ap-south-1.amazonaws.com/site/content/blog/v1591187706/Blog_Post_01.jpg"
+                style="width:100%">
+            <div class="text">Hey! Need a Ride?</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="https://onde-website-bucket.s3.ap-south-1.amazonaws.com/site/content/blog/v1591187712/Blog_Post_02.jpg"
+                style="width:100%">
+            <div class="text">Enjoy the Ride!</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="https://onde-website-bucket.s3.ap-south-1.amazonaws.com/site/content/blog/v1591187717/Blog_Post_03.jpg"
+                style="width:100%">
+            <div class="text">Let's Go :)</div>
+        </div>
+
+        <a class="prev" onclick="plusSlides(-1)">❮</a>
+        <a class="next" onclick="plusSlides(1)">❯</a>
+
+    </div>
+    <br>
+
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
+    <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+            showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+            showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) { slideIndex = 1 }
+            if (n < 1) { slideIndex = slides.length }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    </script>
+    <header>
+        <nav>
+            <div class="logo">SHOHOJATRI</div>
+        </nav>
+
+    </header>
+    <div class="section">
+        <h1>
+            Ride Request For You
+        </h1>
+
+        <table>
+            <tr>
+                <td>4 People</td>
+                <td>Jatrabari to Uttara</td>
+            </tr>
+            <tr>
+                <td>18 Aug</td>
+                <td>Pick time: 7:00 AM</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button> BID</button></td>
+            </tr>
+        </table>
+
+        <table>
+
+            <tr>
+                <td>7 People</td>
+                <td>Jatrabari to Motijheel</td>
+            </tr>
+            <tr>
+                <td>18 Aug</td>
+                <td>Pick time: 7:00 AM</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button> BID</button></td>
+            </tr>
+        </table>
+        <table>
+
+            <tr>
+                <td>5 People</td>
+                <td>Jatrabari to NSU</td>
+            </tr>
+            <tr>
+                <td>19 Aug</td>
+                <td>Pick time: 7:00 AM</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button> BID</button></td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>

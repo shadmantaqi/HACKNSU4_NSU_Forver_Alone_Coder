8e10d2c5af52d08a1ccf0ebda0c792933af3f353
Md. Zahirul Islam Nahid <zahirul.nahid@northsouth.edu>
2022-08-19 12:02:46 +0600

    -

--- bid.php
+++ bid.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
         * {
             margin: 0;
@@ -30,6 +31,7 @@
             color: #fff;
             text-align: center;
         }
+
 
         header img {
             width: 100%;
@@ -191,10 +193,54 @@
             }
         }
     </style>
+
+<style>
+body {margin:0;}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+li {
+  float: left;
+}
+
+li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+li a:hover:not(.active) {
+  background-color: #111;
+}
+
+.active {
+  background-color: #04AA6D;
+}
+</style>
 </head>
 
 <body>
+<header>
+<ul>
+  <li><a class="active" href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="#about">About</a></li>
+</ul>
 
+
+    </header>
     <div class="slideshow-container">
 
         <div class="mySlides fade">
@@ -258,12 +304,7 @@
             dots[slideIndex - 1].className += " active";
         }
     </script>
-    <header>
-        <nav>
-            <div class="logo">SHOHOJATRI</div>
-        </nav>
 
-    </header>
     <div class="section">
         <h1>
             Ride Request For You
--- protection.php
+++ protection.php
@@ -1,6 +1,6 @@
 <?php
  session_start();
- echo $_SESSION["loggedin"];
+
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;

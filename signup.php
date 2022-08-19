119db650a11d7beb2642a23b65ce3bb9a45f07f6
Md. Labib Hasan <59532072+Labib25@users.noreply.github.com>
2022-08-19 11:16:28 +0600

    sign up updated

--- signup.php
+++ signup.php
@@ -96,9 +96,7 @@
 
             <label for="psw-repeat"><b>Repeat Password</b></label>
             <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-            <label>
-                <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-            </label>
+        
             <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
             <div class="clearfix">
                 <button type="button" class="cancelbtn">Cancel</button>

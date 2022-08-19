ebf24c33c9b57a055b22f3124364c309488018c2
Md. Zahirul Islam Nahid <zahirul.nahid@northsouth.edu>
2022-08-19 14:09:17 +0600

    2 PM

--- car.php
+++ car.php
@@ -32,22 +32,50 @@ div {
 <body>
 
 <h3>Car</h3>
+<?php
+               if(isset($_POST["seat"])){   $updateQ="UPDATE `car` SET ".implode(",",$updateQ1)." Where `driver_id`=".$_SESSION["userid"];
+              $sql = $updateQ;
 
+if ($conn->query($sql) === TRUE) {
+echo "Record updated successfully";
+} else {
+echo "Error updating record: " . $conn->error;
+}
+             }
+
+$sql = "SELECT * FROM `car` WHERE `driver_id`=".$_SESSION["userid"];
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $id=$row["id"];
+    $model=$row["model"];
+    $noplate=$row["noplate"];
+    $ac=$row["ac"];
+    $type=$row["type"];
+    $seat=$row["seat"];
+    $driver_id=$row["driver_id"];
+
+              }
+            } else {
+              echo "0 results";
+            }
+?>
 <div>
-  <form>
+  <form method="POST">
   <label for="noplate">Car No. plate:</label><br>
-  <input type="int" id="noplate" name="noplate" value="" placeholder="Car No. plate" pattern=""><br><br>
+  <input type="text" id="noplate" name="noplate" value="<?php echo $noplate?>" placeholder="Car No. plate" ><br><br>
   <label for="model">Model:</label><br>
-  <input type="text" id="model" name="model" value="" placeholder="Model" pattern=""<br><br>
+  <input type="text" id="model" name="model" value="<?php echo $model?>" placeholder="Model" <br><br>
   <label for="Seat No:">Seat No::</label><br>
-  <input type="text" id="Seat No:" name="Seat No:" value="" placeholder="Seat No:" pattern=""<br><br>
+  <input type="text" id="Seat No:" name="seat" value="<?php echo $seat?>" placeholder="Seat No:" ><br><br>
   <p>Type:</p>
-<form action="/action_page.php">
-  <input type="radio" id="html" name="type" value="AC">
+
+  <input type="radio" id="html" name="type" value="AC" <?php if($type=="AC")echo "checked"?>>
   <label for="type">AC</label><br>
-  <input type="radio" id="Non AC" name="type" value="Non AC">
+  <input type="radio" id="Non AC" name="type" value="Non" <?php if($type=="Non")echo "checked"?>>
   <label for="type">Non AC</label><br>
-  
    <input type="submit" value="Submit">
 </form>
 </div>
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 include'protection.php';$driver="";if(isset($_SESSION["account_as"]))$driver=$_SESSION["account_as"];
-if($driver=='driver')include 'bid.php';
+if($driver=='driver')include 'dashboadDriver.php';
+if($driver=='passenger')include 'dashboardPassenger.php';
 
 ?>
